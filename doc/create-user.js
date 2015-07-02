0d7849cc1fbf150cbedf72ca35f7926bea0c52d6
Scotty Logan <swl@stanford.edu>
2015-07-02 08:47:51 -0700

    added create user example

---
+++ doc/create-user.js
@@ -0,0 +1,33 @@
+var fs          = require('fs'),
+    config      = JSON.parse(fs.readFileSync('test/config.json', 'utf8')),
+    gitlab      = require('../lib/gitlab.js')(config.url, config.token),
+    user        = {
+      "email":    "lelandjr@itlab.stanford.edu",
+      "username": "lelandjr",
+      "name":     "Leland Stanford, Jr",
+      "provider": "saml",
+      "extern_uid": "lelandjr@itlab.stanford.edu"
+    };
+
+gitlab.users.create(user).done(
+  function (newUser) {
+    user.id = newUser.id;
+    console.dir(newUser);
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
+gitlab.users.addSSHKey(
+  user.id, 'lelandjr',
+  'ssh-rsa AAAAB...'
+ ).done(
+  function (key) {
+    console.dir(key);
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
